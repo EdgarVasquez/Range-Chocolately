7f873094312a38a8a80b7d0ed241d2fd88ad0426
EdgarVasquez <edgarvasquezpe@gmail.com>
2022-07-05 21:21:28 -0400

    commit 1

---
+++ Range_1098586_1095504.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir '\src\dist\main.exe'
+
+$packageArgs = @{
+  packageName   = 'Range_1098586_1095504.portable'
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+   file         = $fileLocation
+
+  softwareName  = 'Range_1098586_1095504.portable*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  # Checksums are now required as of 0.10.0.
+  # To determine checksums, you can get that from the original site if provided.
+  # You can also use checksum.exe (choco install checksum) and use it
+  # e.g. checksum -t sha256 -f path\to\file
+  checksum      = ''
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = ''
+  checksumType64= 'sha256' #default is checksumType
+
+  # MSI
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+ }
+
+Install-ChocolateyPackage @packageArgs 
